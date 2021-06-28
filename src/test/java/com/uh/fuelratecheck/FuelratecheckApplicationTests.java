61cabe451f93888cf10e1527bb1e6b9f5ca65701
Mio Waller <mimi.w.224@gmail.com>
2021-06-28 11:51:59 -0500

    Added a login form.

---
+++ src/test/java/com/uh/fuelratecheck/FuelratecheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uh.fuelratecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelratecheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
