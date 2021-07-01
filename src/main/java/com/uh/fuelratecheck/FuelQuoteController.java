a26f40e63967da8536acb48ec61220e2169ad202
calvincycle <79472699+calvincycle@users.noreply.github.com>
2021-07-01 08:45:55 +0300

    Update FuelQuoteController.java

--- src/main/java/com/uh/fuelratecheck/FuelQuoteController.java
+++ src/main/java/com/uh/fuelratecheck/FuelQuoteController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FuelQuoteController {
     @GetMapping("/fuelquote")
-	public String history() {
+	public String fuelquote) {
         return "fuelquote";
 	}
 }
