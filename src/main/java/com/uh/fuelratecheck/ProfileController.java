3160f0dad337ee8325fede7cb2b84bd9b87d4bef
calvincycle <79472699+calvincycle@users.noreply.github.com>
2021-07-12 21:40:16 +0300

    a
    a

--- src/main/java/com/uh/fuelratecheck/ProfileController.java
+++ src/main/java/com/uh/fuelratecheck/ProfileController.java
@@ -7,6 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ProfileController {
     @GetMapping("/profile")
 	public String profile() {
+        ClientProfileManagement client = new ClientProfileManagement();
         return "profile";
 	}
+
+    @PostMapping("/profile")
+    public String setupProfile(@ModelAttribute ClientProfileManagement client) {
+
+
+
+        // if (login.getUsername().equals("mimi") && login.getPassword().equals("mypass")) {
+        //     return "redirect:/fuelhistory";
+        // } else {
+        //     return "redirect:/login";
+        // }
+
+        return 0;
+    }
 }
