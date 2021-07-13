6a90b9fc40995c25d0cb946c682c68df73077087
Robert120297 <robertgreen500@gmail.com>
2021-07-13 16:13:00 -0500

    Revert "Merge branch 'Akif' of https://github.com/MioWaller/fuelratecheck-spring into Akif"
    This reverts commit 4dc9d3b636e8c0fa75f7a3801f125691dead73af, reversing
    changes made to f245558caac6b0c376f12b17fff47086f188428d.

--- src/main/java/com/uh/fuelratecheck/LoginController.java
+++ src/main/java/com/uh/fuelratecheck/LoginController.java
@@ -1,29 +1,12 @@
 package com.uh.fuelratecheck;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-	public String login(Model model) {
-        LoginModel loginModel = new LoginModel();
-        model.addAttribute("login", loginModel);
+	public String login() {
         return "login";
 	}
-
-    @PostMapping("/login")
-    public String loginSubmit(@ModelAttribute LoginModel login) {
-        if (login.getUsername().equals("mimi") && login.getPassword().equals("mypass")) {
-            return "redirect:/fuelhistory";
-        }
-        
-        else {
-            return "redirect:/login";
-        }
-    }
-
 }
