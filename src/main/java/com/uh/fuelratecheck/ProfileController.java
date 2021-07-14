2c41b21ece9ae0a835bf1a1b554ae542432721cd
calvincycle <79472699+calvincycle@users.noreply.github.com>
2021-07-14 20:14:20 +0300

    ClientProfileManagement, profile, ProfileController

--- src/main/java/com/uh/fuelratecheck/ProfileController.java
+++ src/main/java/com/uh/fuelratecheck/ProfileController.java
@@ -1,12 +1,25 @@
 package com.uh.fuelratecheck;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfileController {
     @GetMapping("/profile")
-	public String profile() {
+	public String profile(Model model) {
+        ClientProfileManagement client = new ClientProfileManagement();
+        model.addAttribute("profile", client);
         return "profile";
 	}
+
+    @PostMapping("/profile")
+    public String setupProfile(@ModelAttribute ClientProfileManagement client) {
+
+        //this method is probably unnecessary, so leaving it blank for now
+
+        return "";
+    }
 }
