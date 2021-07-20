581fcfea964930a4ff59389e8c4f422dc4a642cf
Mio Waller <mimi.w.224@gmail.com>
2021-07-20 02:36:12 -0500

    Update RegistrationController.java

--- src/main/java/com/uh/fuelratecheck/RegistrationController.java
+++ src/main/java/com/uh/fuelratecheck/RegistrationController.java
@@ -1,13 +1,18 @@
 package com.uh.fuelratecheck;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegistrationController {
+    @Autowired
+    private ClientRepository clientRepository;
+
     @GetMapping("/registration")
 	public String registration(Model model) {
         RegistrationModel registrationModel = new RegistrationModel();
@@ -16,13 +21,18 @@ public class RegistrationController {
 	}
     
     @PostMapping("/registration")
-    public String registrationSubmit(@ModelAttribute RegistrationModel registration) {
+    public String registrationSubmit(@ModelAttribute RegistrationModel registration, @RequestParam String username
+    , @RequestParam String password) {
         if (registration.getUsername().equals("") || registration.getPassword().equals("")) {
             return "redirect:/registration";
         }
 
         else {
+            ClientEntity n = new ClientEntity();
+            n.setName(username);
+            n.setPassword(password);
+            clientRepository.save(n);
             return "redirect:/profile";
-        }  
-    }   
+        } 
+    } 
 }
