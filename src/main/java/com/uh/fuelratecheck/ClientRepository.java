f930bd5166dbb1855a700ca424375768df8d4efa
Mio Waller <mimi.w.224@gmail.com>
2021-07-20 02:36:03 -0500

    Create ClientRepository.java

---
+++ src/main/java/com/uh/fuelratecheck/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.uh.fuelratecheck;
+
+import org.springframework.data.repository.CrudRepository;
+
+//import com.uh.fuelratecheck.ClientEntity;
+
+public interface ClientRepository extends CrudRepository<ClientEntity, Integer> {
+    
+
+}
