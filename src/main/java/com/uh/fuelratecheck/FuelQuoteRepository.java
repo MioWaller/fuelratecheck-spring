0fb0090c444ace3add915becc1f449a2a1c581b1
Robert120297 <robertgreen500@gmail.com>
2021-07-24 00:54:15 -0500

    :(
    Still working on getting my program to connect to my DB

--- src/main/java/com/uh/fuelratecheck/FuelQuoteRepository.java
+++ src/main/java/com/uh/fuelratecheck/FuelQuoteRepository.java
@@ -1,9 +1,11 @@
 package com.uh.fuelratecheck;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.uh.fuelratecheck.FuelQuoteModel;
 
+@Repository
 public interface FuelQuoteRepository extends CrudRepository<FuelQuoteModel, Integer> {
     
 
