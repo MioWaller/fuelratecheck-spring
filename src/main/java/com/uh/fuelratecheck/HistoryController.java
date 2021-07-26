25b964c5b0d8f1c83710df2c4ae353e1dc22ce49
Robert120297 <robertgreen500@gmail.com>
2021-07-26 00:29:36 -0500

    FuelHistory now shows only history of current user

--- src/main/java/com/uh/fuelratecheck/HistoryController.java
+++ src/main/java/com/uh/fuelratecheck/HistoryController.java
@@ -4,14 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.*;
 
 @Controller
 public class HistoryController {
     @Autowired
     private FuelQuoteRepository fuelQuoteRepository;
     @GetMapping("/fuelhistory")
-    public String history(Model model) {
-        model.addAttribute("quotes", fuelQuoteRepository.findAll());
+
+    public String history(Model model, HttpServletRequest request) {
+        Cookie cookie[] = request.getCookies();
+        String userid = cookie[0].getValue();
+        model.addAttribute("quotes", fuelQuoteRepository.findByUserid(Integer.parseInt(userid)));
         return "fuelhistory";
     }
 }
