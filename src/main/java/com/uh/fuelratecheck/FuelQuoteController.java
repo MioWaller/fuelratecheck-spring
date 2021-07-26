01548edc1792253373fcc7a921ec35538c844fe0
Robert120297 <robertgreen500@gmail.com>
2021-07-26 14:36:33 -0500

    FuelQuoteController now received address from client_info_entity table

--- src/main/java/com/uh/fuelratecheck/FuelQuoteController.java
+++ src/main/java/com/uh/fuelratecheck/FuelQuoteController.java
@@ -6,11 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import javax.servlet.http.*;
+import java.util.List;
 
 @Controller
 public class FuelQuoteController {
     @Autowired
 	private FuelQuoteRepository fuelQuoteRepository;
+    @Autowired
+    private ClientInfoRepository clientInfoRepository;
 
     @GetMapping("/fuelquote")
         public String fuelquote(Model model) {
@@ -39,9 +42,11 @@ public class FuelQuoteController {
                 break;
             }
         }
+            List<ClientInfoEntity> clientInfoEntity = clientInfoRepository.findByUserid(Integer.parseInt(userid));
             FuelQuoteEntity n = new FuelQuoteEntity();
             n.setgallonsRequested(gallonsRequested);
             n.setdeliveryDate(deliveryDate);
+            n.setdeliveryAddress(clientInfoEntity.get(0).getAddress1());
             n.setUserId(Integer.parseInt(userid));
             fuelQuoteRepository.save(n);
             return "redirect:/fuelquote";
