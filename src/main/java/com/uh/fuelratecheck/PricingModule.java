c831ded26bedf664948d6e876932623ac18b7750
calvincycle <79472699+calvincycle@users.noreply.github.com>
2021-08-01 19:11:33 -0500

    a

---
+++ src/main/java/com/uh/fuelratecheck/PricingModule.java
@@ -0,0 +1,15 @@
+package com.uh.fuelratecheck;
+
+public class PricingModule {
+    
+    public static double calculateSuggestedPrice(){
+
+        return 0;
+    }
+
+    public static double calculateTotalPrice(){
+        
+        return 0;
+    }
+
+}
