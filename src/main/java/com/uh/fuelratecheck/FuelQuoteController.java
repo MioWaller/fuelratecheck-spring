192449f348f77325bf5d2b94ade21f826fd1f09a
Mio Waller <mimi.w.224@gmail.com>
2021-08-05 21:03:50 -0500

    Update FuelQuoteController.java

--- src/main/java/com/uh/fuelratecheck/FuelQuoteController.java
+++ src/main/java/com/uh/fuelratecheck/FuelQuoteController.java
@@ -17,26 +17,23 @@ public class FuelQuoteController {
     PricingModule m = new PricingModule();
 
     @GetMapping("/fuelquote")
-        public String fuelquote(Model model, HttpServletRequest request) {
+    public String fuelquote(Model model, HttpServletRequest request) {
         LocalDate now = LocalDate.now();
+        LocalDate later = now.plusDays(14);
         model.addAttribute("now", now);
-        FuelQuoteEntity fuelQuoteModel = new FuelQuoteEntity();
-        model.addAttribute("fuelquote", fuelQuoteModel);
 
         //get cookies to find out which user is editing their client info
         Cookie cookie1[] = request.getCookies();
         String userid="";
         for(int i=0; i<cookie1.length; i++) {
             userid = cookie1[i].getValue();
-            try{
+            try {
                 Integer.parseInt(userid);
             }
-            catch(NumberFormatException e)
-            {
+            catch(NumberFormatException e) {
                 userid=null;
             }
-            if(userid != null)
-            {
+            if(userid != null) {
                 break;
             }
         }
@@ -45,11 +42,53 @@ public class FuelQuoteController {
 
         model.addAttribute("entities", clientInfoEntity);
 
+        FuelQuoteForm form = new FuelQuoteForm();
+        form.gallonsRequested = 100;
+        form.deliveryDate = later;
+
+        model.addAttribute("quoteForm", form);
+        model.addAttribute("quotePrice", "");
+
         return "fuelquote";
-    }   
-    @PostMapping("/fuelquote")
-        public String addNewUser (@ModelAttribute FuelQuoteEntity fuelquote, @RequestParam String gallonsRequested
-      , @RequestParam String deliveryDate, HttpServletRequest request) {
+    }
+
+    @RequestMapping(value = "/fuelquote", method = RequestMethod.POST, params = "getquote")
+    public String getFuelQuote(Model model, @ModelAttribute FuelQuoteForm form, HttpServletRequest request) {
+        //get cookies to find out which user is editing their client info
+        Cookie cookie1[] = request.getCookies();
+        String userid="";
+        for(int i=0; i<cookie1.length; i++) {
+            userid = cookie1[i].getValue();
+            try {
+                Integer.parseInt(userid);
+            }
+            catch(NumberFormatException e) {
+                userid=null;
+            }
+            if(userid != null) {
+                break;
+            }
+        }
+        // Get the client info for the userId from the database.
+        List<ClientInfoEntity> clientInfoEntity = clientInfoRepository.findByUserid(Integer.parseInt(userid));
+
+        model.addAttribute("entities", clientInfoEntity);
+
+        LocalDate now = LocalDate.now();
+        model.addAttribute("now", now);
+
+        model.addAttribute("quoteForm", form);
+        model.addAttribute("quotePrice", "$4000000000");
+        
+        // Use the pricing module to calculate the quote.
+
+        // Added both the parameters and the result to the model via addAttributes (quotePrice, gallonsRequested, deliveryDate)
+
+        return "fuelquote";
+    }
+    
+    @RequestMapping(value = "/fuelquote", method = RequestMethod.POST, params = "savequote")
+    public String addNewUser (@ModelAttribute FuelQuoteEntity fuelquote, @RequestParam String gallonsRequested, @RequestParam String deliveryDate, HttpServletRequest request) {
         Cookie cookie[] = request.getCookies();
         String userid="";
         for(int i=0; i<cookie.length; i++) {
@@ -57,31 +96,32 @@ public class FuelQuoteController {
             try{
                 Integer.parseInt(userid);
             }
-            catch(NumberFormatException e)
-            {
+            catch(NumberFormatException e) {
                 userid=null;
             }
-            if(userid != null)
-            {
+            if(userid != null) {
                 break;
             }
         }
 
-            m.setgallonsRequested(gallonsRequested);
-            m.setuserid(userid);
+        // TODO: Validate the inputs
+        // Check the date, # of gallons requested
 
-            List<ClientInfoEntity> clientInfoEntity = clientInfoRepository.findByUserid(Integer.parseInt(userid));
-            FuelQuoteEntity n = new FuelQuoteEntity();
-            String getFullAddress = (clientInfoEntity.get(0).getAddress1() + ", " + clientInfoEntity.get(0).getCity()
-            + ", " + clientInfoEntity.get(0).getState() + " " + clientInfoEntity.get(0).getZipcode());
-            n.setgallonsRequested(gallonsRequested);
-            n.setdeliveryDate(deliveryDate);
-            n.setdeliveryAddress(getFullAddress);
-            n.setUserId(Integer.parseInt(userid));
-            n.setsuggestedPrice(String.valueOf(m.calculateSuggestedPrice()));
-            n.settotalPrice(String.valueOf(m.calculateTotalPrice()));
-            fuelQuoteRepository.save(n);
-            return "redirect:/fuelquote";
+        m.setgallonsRequested(gallonsRequested);
+        m.setuserid(userid);
+
+        List<ClientInfoEntity> clientInfoEntity = clientInfoRepository.findByUserid(Integer.parseInt(userid));
+        FuelQuoteEntity n = new FuelQuoteEntity();
+        String getFullAddress = (clientInfoEntity.get(0).getAddress1() + ", " + clientInfoEntity.get(0).getCity()
+        + ", " + clientInfoEntity.get(0).getState() + " " + clientInfoEntity.get(0).getZipcode());
+        n.setgallonsRequested(gallonsRequested);
+        n.setdeliveryDate(deliveryDate);
+        n.setdeliveryAddress(getFullAddress);
+        n.setUserId(Integer.parseInt(userid));
+        n.setsuggestedPrice(String.valueOf(m.calculateSuggestedPrice()));
+        n.settotalPrice(String.valueOf(m.calculateTotalPrice()));
+        fuelQuoteRepository.save(n);
+        return "redirect:/fuelquote";
     }
 
 }
