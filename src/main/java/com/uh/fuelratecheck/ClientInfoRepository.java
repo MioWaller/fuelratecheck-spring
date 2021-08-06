f9d84e590e00735aec22e8d412a112476b2161ba
Mio Waller <mimi.w.224@gmail.com>
2021-08-05 21:18:41 -0500

    Deleted All Imports That Were Not Being Used

--- src/main/java/com/uh/fuelratecheck/ClientInfoRepository.java
+++ src/main/java/com/uh/fuelratecheck/ClientInfoRepository.java
@@ -1,7 +1,6 @@
 package com.uh.fuelratecheck;
 
 import org.springframework.data.repository.CrudRepository;
-import com.uh.fuelratecheck.ClientInfoEntity;
 import java.util.List;
 
 public interface ClientInfoRepository extends CrudRepository<ClientInfoEntity, Integer> {
--- src/test/java/com/uh/fuelratecheck/ClientInfoEntityTests.java
+++ src/test/java/com/uh/fuelratecheck/ClientInfoEntityTests.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 public class ClientInfoEntityTests {
     @Test
